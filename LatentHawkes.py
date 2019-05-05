b567203f3cb4bd6684a629576e98349a529570b6
Atanasiu Stefan Demian <ad15865.2015@my.bristol.ac.uk>
2019-05-05 16:24:27 +0100

    Update LatentHawkes.py

--- LatentHawkes.py
+++ LatentHawkes.py
@@ -26,7 +26,7 @@ def LatHawkes(T, arrivals, alpha_lmbd, beta_lmbd, alpha_a, beta_a, alpha_b, beta
     """
     Perform Gibbs sampling on parameters lambda, a, b using Gamma priors.
     Parameters a and lambda are sampled directly from their corresponding posterior distributions.
-    Parameter b is samples via a Random Walk Metropolis step 
+    Parameter b is sampled via a Random Walk Metropolis step 
     T = length of observation
     arrivals = dataset of spikes
     alpha_lmbd, beta_lmbd, alpha_a, beta_a, alpha_b, beta_b = parameters of Gamma priors.
@@ -120,12 +120,12 @@ def predictive_likelihood(lmbd, a, b, train, test, T_train, T_test, L=10000):
     """
     Compute Monte Carlo estimate of normalized predictive log-likelihood of Hawkes model as in eq. 3.4.
     As baseline model I used a homogeneous Poisson process of rate given by MLE on train set.
+    lmbd, a, b = sampled parameter distributions.
     """
     
     # First compute homogeneous Poisson MLE
     hpp_mle = len(train)/T_train
     hpp_log_likelihood = -hpp_mle*T_test + len(test)*np.log(hpp_mle)
-    likelihood_dist = []
     sum_log_likelihood = 0.0
     for l in range(L): 
         post_lmbd = np.random.choice(lmbd)
@@ -134,7 +134,6 @@ def predictive_likelihood(lmbd, a, b, train, test, T_train, T_test, L=10000):
         estimate = -post_lmbd*T_test + log_exp_term(post_a, post_b, T_test, test)+log_prod_CIF(
                     post_lmbd, post_a, post_b, test)
         sum_log_likelihood += estimate
-        likelihood_dist.append(estimate)
     mean_log_likelihood = sum_log_likelihood/L
     
-    return (mean_log_likelihood - hpp_log_likelihood)/len(test), (np.array(likelihood_dist)-hpp_log_likelihood)/len(test)
+    return (mean_log_likelihood - hpp_log_likelihood)/len(test)
