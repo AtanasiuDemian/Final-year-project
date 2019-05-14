819fc519d5ab5c29391cfce8c7d489aa6477cf55
Atanasiu Stefan Demian <ad15865.2015@my.bristol.ac.uk>
2019-05-14 20:38:42 +0100

    Update AllFilters.py

--- AllFilters.py
+++ AllFilters.py
@@ -4,52 +4,55 @@
 # 3. SMC with linear approximation of the optimal importance distribution p(x_{t}|x_{t-1}, y_{t}) - see Doucet et al, 2000.
 # In all three implementations, if weights are all zero then algorithm returns marginal likelihood of -infinity. This is useful when
 # run with PMMH, because a candidate with likelihood of -infinity is automatically rejected.
+# These algorithms use Poisson distribution of spike counts, but for small intensity they give same results as Bernoulli.
 
-def bootstrap_filter(Y, N, T, alpha, b, sigmasq, delta):
+def bootstrap_filter(Y, N, T, alpha, b, scale, delta, time_sections=[], mult=1):
     '''
     Implement boostrap filter, as described in Doucet, de Freitas & Gordon, 2001.
-    Returns particles, one-step ahead distribution, filtering distribution, log marginal likelihood p(y_{1:t})
-    and normalized weights.
-    See Andrieu et al,2010 for estimation of marginal likelihood p(y_{1:t}).
-    This code was used for simulated decoding in Sec. 6.3, and assumes all initial hidden states are 0.
+    Returns resampled particles after N iterations, marginal likelihood p(y_{1:t})
     '''
-    
+    hists, h = np.zeros((len(time_sections), N)), 0
     log_marginal_likelihood = 0.0
     particles = np.zeros((N, T+1))
     #particles[:,0] = norm.rvs(scale=np.sqrt(scale**2/(1-alpha**2)), size=N)
     particles[:,0] = np.zeros((N,))
     preds, estimates = [], []
     pred_dist = np.zeros((N, T))
-    pred_dist[:,0] = alpha*particles[:,0]+norm.rvs(scale=np.sqrt(sigmasq), size=N)
-    resampled = False
+    pred_dist[:,0] = alpha*particles[:,0]+scale*norm.rvs(size=N)
     prev_weights = np.ones((N,))/N
     for t in range(1,T+1):
-        
+
         weights = np.ones((N,))/N
         for i in range(N):
             prev_x = particles[i, t-1]
-            rand_eps = norm.rvs(scale=np.sqrt(sigmasq))
+            rand_eps = scale*norm.rvs()
             curr_x = alpha*prev_x + rand_eps
             particles[i, t] = curr_x
 
-            cif = b*np.exp(curr_x)
-            weights[i] = bernoulli.pmf(Y[t-1], p=min(1, cif*delta))
+            cif = b*np.exp(mult*curr_x)
+            weights[i] = poisson.pmf(Y[t-1], mu=cif*delta)
         
+        if np.sum(weights)<=0:
+            print('all weights are zero for alpha, b, scale : {}, {}, {}'.format(alpha, b, scale))
+            log_marginal_likelihood = -float('inf')
+            return particles, pred_dist, estimates, log_marginal_likelihood, weights
         normalized_weights = weights/np.sum(weights)
         log_marginal_likelihood += np.log(np.mean(weights))
-        estimates.append(np.dot(normalized_weights, particles[:,t]))  # filtering distribution
+        estimates.append(np.dot(normalized_weights, particles[:,t]))
+
         # Now resample
         particles_before_resampling = particles[:, range(t+1)]    # Use range in order to create copy of array!
         for j in range(N):
             msk = np.random.choice(range(N), p=normalized_weights, replace=True)
             particles[j, 0:t+1] = particles_before_resampling[msk, 0:t+1]
-        
-        # Compute the one-step ahead prediction distribution of the next hidden state.
+
+        # Predict 
         if t<T:
-            pred_dist[:,t] = alpha*particles[:,t]+norm.rvs(scale=np.sqrt(sigmasq), size=N)
+            pred_dist[:,t] = alpha*particles[:,t]+scale*norm.rvs(size=N)
             preds.append(np.mean(pred_dist[:,t]))
 
     return particles, pred_dist, estimates, log_marginal_likelihood, normalized_weights
+
 
 
 def pred_filter(Y, N, T, alpha, b, scale, delta):
@@ -72,8 +75,8 @@ def pred_filter(Y, N, T, alpha, b, scale, delta):
                 particles[i,t] = curr_x
 
                 cif = b*np.exp(curr_x)
-                weights[i] = poisson.pmf(Y[t-1], mu=cif*delta)#*norm.pdf(curr_x, loc=alpha*prev_x, scale=scale)
-
+                weights[i] = poisson.pmf(Y[t-1], mu=cif*delta)
+                
             if np.sum(weights)<=0: 
                 print('all weights are zero for alpha, b, scale : {}, {}, {}'.format(alpha, b, scale))
                 log_marginal_likelihood = -float('inf')
